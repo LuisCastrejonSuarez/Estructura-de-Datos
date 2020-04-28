3280bc2a67b82f1bee6fab27a5424788f2d596ff
Luis Monster <luis@big-monster.net>
2020-04-28 09:48:05 -0500

    setup combo detector

--- Assets/2. Queue/Scripts/ComboDetector.cs
+++ Assets/2. Queue/Scripts/ComboDetector.cs
@@ -1,18 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ComboDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Public
+    public float tolerance = 0.5f;
+    public int bufferSize = 4;
+    public string debug;
+
+    // Private
+    private float counter;
+    private Queue<KeyValuePair<float, string>> buffer;
+    private string[] combo1 = new string[] { "P", "right", "down" };
+    private string[] combo2 = new string[] { "K", "right", "down" };
+
+    void Awake()
     {
-        
+        buffer = new Queue<KeyValuePair<float, string>>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown)
+        {
+            counter = tolerance;
+        }
+        else if (counter != -1)
+        {
+            counter -= Time.deltaTime;
+        }
+        if(counter<0)
+        {
+            ResetCombo();
+        }
+
+        //Enqueue
+
+        DetectInput();
+
+        //Detection
+        // ToDo: Verificar combo
+        Verify();
+
+        //Dequeue
+
+        if (buffer.Count > bufferSize)
+            buffer.Dequeue();
+    }
+
+    private void Verify()
+    {
+        buffer.Peek();
+        buffer.Dequeue();
+    }
+
+    private void DetectInput()
+    {
+        // ToDo: Enqueu de las demás teclas
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            buffer.Enqueue(new KeyValuePair<float, string>(Time.time, "down"));
+            debug += "down, ";
+        }
+    }
+
+    private void ResetCombo()
+    {
+        buffer = new Queue<KeyValuePair<float, string>>();
+        debug = string.Empty;
+        counter = -1;
     }
 }
---
+++ Assets/2. Queue/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    /// <summary>
+    /// Inputs
+    /// - Punches: Q,W,E
+    /// - Kicks: A,S.D
+    /// - Movement: Arrows
+    /// </summary>
+
+    // Private
+
+    private ComboDetector detector;
+
+    void Awake()
+    {
+        detector = GetComponent<ComboDetector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            Debug.Log("Q");
+        if (Input.GetKeyDown(KeyCode.W))
+            Debug.Log("W");
+        if (Input.GetKeyDown(KeyCode.E))
+            Debug.Log("E");
+        if (Input.GetKeyDown(KeyCode.A))
+            Debug.Log("A");
+        if (Input.GetKeyDown(KeyCode.S))
+            Debug.Log("S");
+        if (Input.GetKeyDown(KeyCode.D))
+            Debug.Log("D");
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            Debug.Log("Down");
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            Debug.Log("Left");
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            Debug.Log("Right");
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            Debug.Log("Up");
+    }
+}
